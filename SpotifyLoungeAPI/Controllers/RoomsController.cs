92ec45f16b1ad01dbafb46adccd1e09e1f6393c8
Parker Johnson <carverj95@gmail.com>
2022-08-20 11:11:21 -0400

    add: rooms controller

---
+++ SpotifyLoungeAPI/Controllers/RoomsController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SpotifyLoungeAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RoomsController : ControllerBase
+    {
+        HttpClient _client;
+        public RoomsController(IHttpClientFactory factory)
+        {
+            _client = factory.CreateClient();
+        }
+        [HttpGet]
+        public IActionResult Rooms()
+        {
+            //todo return list of rooms
+            //todo refactor
+            var json = new
+            {
+                response = "test"
+            };
+            return Ok(json);
+        }
+    }
+}
