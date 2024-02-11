8b9722842f56afe424717dc6fd5cb3e52cd688b4
carve <carverj95@gmail.com>
2024-02-11 11:14:55 -0500

    update connection string

--- DBAccess/ConnectionManager.cs
+++ DBAccess/ConnectionManager.cs
@@ -5,7 +5,7 @@ public static class ConnectionManager
     public static string GetConnectionString()
     {
 #if DEBUG
-        return "Server=PJ-LAPTOP;Database=SpotifyApp;Trusted_Connection=True;";
+        return "Server=PJ-COMP;Database=SpotifyApp;Trusted_Connection=True;";
         //return "Server=DESKTOP-9D6BNJG;Database=SpotifyApp;Trusted_Connection=True;";
 #endif
     }
